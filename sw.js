4262f37474cd546fa3d80061b9b70ab2530929ca
Emiliano Milla <milla_e_6535@yahoo.com>
2023-01-26 22:54:13 -0300

    Modificacion de ruta de sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator)
 {
-    navigator.serviceWorker.register('/sw/sw.js', {scope: '/../'}).then((registration) => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
         console.log('Service worker registration succeeded: ', registration);
     }, (error) => {
         console.log('Service worker registration failed: ${error}');
